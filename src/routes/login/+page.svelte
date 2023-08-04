e878b4de3fd58af8d99a84c4132e130965335943
CTAPCKPIM <novickdanya@gmail.com>
2023-08-04 19:57:13 +0300

    feat: creating the interface for the login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -26,10 +26,49 @@
   };
 </script>
 
-{#if data.user}
-  <p>Hello <b>{data.user.name}</b></p>
-  <button on:click|preventDefault={logout} disabled={isRequesting}>Logout</button>
-{:else}
-  <button on:click|preventDefault={loginWithGithub} disabled={isRequesting}
-    >Log in with Github</button>
-{/if}
+<style type="text/css">
+  .container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(16, 20, 31, 1);
+  }
+
+  .button {
+    font-weight: bold;
+    font-family: "Inter", sans-serif;
+    font-size: 13.9px;
+    width: 156px;
+    height: 48px;
+    padding: 8px 16px;
+    border-radius: 12px;
+    border: none;
+    cursor: pointer;
+    box-shadow:
+      0px 2px 0px 0px rgba(255, 255, 255, 0.24) inset,
+      0px 4px 8px 0px rgba(0, 0, 0, 0.16),
+      0px 0px 0px 2px rgba(0, 0, 0, 0.2);
+    transition: background-color 0.3s;
+    background: rgba(0, 204, 255, 1);
+    color: rgba(16, 20, 31, 1);
+  }
+
+  .button:hover {
+    background: rgba(84, 204, 255, 1);
+  }
+</style>
+
+<div class="container">
+  {#if data.user}
+    <p>Hello <b>{data.user.name}</b></p>
+    <button class="button" on:click|preventDefault={logout} disabled={isRequesting}>Logout</button>
+  {:else}
+    <button class="button" on:click|preventDefault={loginWithGithub} disabled={isRequesting}
+      >Log in with Github</button>
+  {/if}
+</div>
