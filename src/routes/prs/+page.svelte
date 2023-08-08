8c788279b4190a64225f5464f694bb3cd50ffaeb
CTAPCKPIM <novickdanya@gmail.com>
2023-08-08 17:30:09 +0300

    init

--- src/routes/prs/+page.svelte
+++ src/routes/prs/+page.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
   /** internals */
   import Card from '$lib/components/Card/index.svelte';
+  import Toggle from '$lib/components/Toggle/index.svelte';
 
   /** vars */
 </script>
 
 <main class="max-w-container m-auto p-4 sm:py-8">
-  <ul class="grid gap-4 sm:gap-8">
-    <Card />
-    <Card isReadonly />
-    <Card isAdmin />
-  </ul>
+  <div class="grid gap-4 sm:gap-8">
+    <Toggle leftButtonProps={{ text: 'Unsubmitted' }} rightButtonProps={{ text: 'Submitted' }} />
+    <Toggle isReactionToggle />
+    <div />
+    <ul class="grid gap-4 sm:gap-8">
+      <Card />
+      <Card isReadonly />
+      <Card isAdmin />
+    </ul>
+  </div>
 </main>
