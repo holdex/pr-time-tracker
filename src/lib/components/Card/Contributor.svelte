fdae883fb028233bb0d2faf0251833e933dff950
CTAPCKPIM <novickdanya@gmail.com>
2023-08-09 18:43:20 +0300

    feat: creating components 'Contributor' and 'Avatar'

---
+++ src/lib/components/Card/Contributor.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  /** vars */
+  let avatarFallback = false;
+
+  /** functs */
+  const handleAvatarError = () => {
+    avatarFallback = true;
+  };
+
+  const redirectToContributorDetails = () => {
+    return 0;
+  };
+</script>
+
+<a class="block cursor-pointer" on:click={redirectToContributorDetails}>
+  <div class="flex items-center space-x-4">
+    {#if !avatarFallback}
+      <img
+        src="https://avatars.githubusercontent.com/u/28790485?v=4"
+        alt="Contributor Avatar"
+        width="32"
+        height="32"
+        on:error={handleAvatarError}
+        class="w-12 h-12 rounded-full" />
+    {:else}
+      <div class="w-12 h-12 bg-gray-300 rounded-full">
+        <p class="text-white text-center text-lg font-bold">S</p>
+      </div>
+    {/if}
+    <span class="text-white text-t3 font-bold"> Sunday Power Inemesit </span>
+  </div>
+</a>
--- src/routes/contributors/+page.svelte
+++ src/routes/contributors/+page.svelte
@@ -1,3 +1,10 @@
-<ul class="grid gap-4 md:gap-8">
-  <li>List of Contributors</li>
-</ul>
+<script lang="ts">
+  /** internals */
+  import Contributor from '$lib/components/Card/Contributor.svelte';
+</script>
+
+<header>
+  <ul class="grid gap-4 md:gap-8">
+    <li><Contributor /></li>
+  </ul>
+</header>
