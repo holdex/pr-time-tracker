f68425676edd5ec8caff9f202d64bf1673b2f4e4
CTAPCKPIM <novickdanya@gmail.com>
2023-08-10 22:17:34 +0300

    feat: added example, TODO: use example and write needing code

--- src/routes/contributors/[username]/+page.svelte
+++ src/routes/contributors/[username]/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   /** internals */
+  import type { PageData } from './$types';
+  
   import Card from '$lib/components/Card/index.svelte';
+
+  /** vars */
+  export let data: PageData;
 </script>
 
 <main class="max-w-container m-auto py-4 animate-fadeIn md:py-8">
@@ -9,4 +14,8 @@
     <Card isReadonly />
     <Card isAdmin />
   </ul>
+
+  <h1>{data.post.title}</h1>
+  <p>{data.post.content}</p>
+  <!-- TODO: output data from +page.server.ts -->
 </main>
