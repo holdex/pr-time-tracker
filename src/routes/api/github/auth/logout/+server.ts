a38664f6a735a171c1eaa7913a558316c477d250
power-f-god <sundaypower9@gmail.com>
2023-11-01 16:21:29 +0100

    fix: possible fix for "PR item has wrong link" - fix cookie duplicates and path ish; - delete all cookies on logout; - add restriction to prevent user who does not own PR to update the same; - remove `/api/items` `PATCH` endpoint.

--- src/routes/api/github/auth/logout/+server.ts
+++ src/routes/api/github/auth/logout/+server.ts
@@ -7,20 +7,17 @@ import { logout } from '$lib/server/github';
 
 export const GET: RequestHandler = async ({ cookies }) => {
   const accessToken = cookies.get(cookieNames.accessTokenCookieName);
-  if (accessToken) {
-    await logout(accessToken);
-  }
-  cookies.delete(
-    cookieNames.accessTokenCookieName,
-    serializeCookie({
-      expires: new Date(0)
-    })
-  );
-  cookies.delete(
-    cookieNames.refreshTokenCookieName,
-    serializeCookie({
-      expires: new Date(0)
-    })
-  );
+
+  if (accessToken) await logout(accessToken);
+
+  cookies.getAll().map(({ name }) => {
+    cookies.delete(
+      name,
+      serializeCookie({
+        expires: new Date(0)
+      })
+    );
+  });
+
   return json({}, { status: 200 });
 };
