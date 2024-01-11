0e2aa369dcaf67de6d0de75b382884f9fbcea5a1
George Ciubotaru <geeku96@gmail.com>
2024-01-11 14:29:36 +0200

    feat: init commit

---
+++ src/lib/server/mongo/collections/events.collection.ts
@@ -0,0 +1,58 @@
+import type { WithId } from 'mongodb';
+
+import { BaseCollection } from './base.collection';
+
+import { CollectionNames, UserRole, type ContributorSchema } from '$lib/@types';
+
+export class ContributorsCollection extends BaseCollection<ContributorSchema> {
+  async update(_payload: Partial<ContributorSchema>): Promise<WithId<ContributorSchema>> {
+    return await super.update(_payload, {
+      onCreateIfNotExist: (payload) => {
+        if (!payload.role) payload.role = UserRole.CONTRIBUTOR;
+        if (!payload.rate) payload.rate = 1;
+
+        return payload;
+      }
+    });
+  }
+}
+
+export const contributors = new ContributorsCollection(CollectionNames.CONTRIBUTORS, {
+  required: [
+    'id',
+    'login',
+    'name',
+    'url'
+    // 'created_at'
+  ],
+  properties: {
+    id: {
+      bsonType: 'int',
+      description: 'must be provided.'
+    },
+    avatarUrl: {
+      bsonType: 'string'
+    },
+    rate: {
+      bsonType: ['int', 'double'],
+      description: 'must be provided.'
+    },
+    login: {
+      bsonType: 'string',
+      description: 'must be provided.'
+    },
+    name: {
+      bsonType: 'string',
+      description: 'must be provided.'
+    },
+    url: {
+      bsonType: 'string',
+      description: 'must be provided.'
+    },
+    role: { enum: Object.values(UserRole), description: 'must be one of the enum values.' },
+    created_at: {
+      bsonType: 'string',
+      description: 'must be provided.'
+    }
+  }
+});
