41e46f145faf12d301b2d3762968a6315aef6cac
George Ciubotaru <geeku96@gmail.com>
2024-04-12 14:18:21 +0300

    feat: init commit

---
+++ src/routes/prs/[org]/[repo]/[id]/+page.server.ts
@@ -0,0 +1,28 @@
+import { error } from '@sveltejs/kit';
+
+import type { PageServerLoad } from './$types';
+
+import { invalidations } from '$lib/config';
+import { items } from '$lib/server/mongo/collections';
+
+export const load: PageServerLoad = async ({ params, depends, parent }) => {
+  depends(invalidations.user);
+
+  const { user } = await parent();
+  const data = await items.getOneWithSubmission(
+    {
+      id: Number(params.id),
+      repo: params.repo,
+      org: params.org
+    },
+    user?.id
+  );
+  if (!data || data.length === 0) {
+    throw error(404, 'pr_not_found');
+  }
+
+  console.log('data', data);
+  return {
+    pr: JSON.stringify(data[0])
+  };
+};
