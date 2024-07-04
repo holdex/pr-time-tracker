0c391403f3e579f1ae81cf63ff68db39c42efad1
George Ciubotaru <geeku96@gmail.com>
2024-07-04 15:27:48 +0300

    chore: optimize pr review jobs

--- src/lib/server/trigger-dev/jobs/pull-requests-review.ts
+++ src/lib/server/trigger-dev/jobs/pull-requests-review.ts
@@ -13,18 +13,17 @@ import {
   getPrInfo
 } from '../utils';
 
-import { EventType } from '$lib/@types';
+import { EventType, type ItemSchema } from '$lib/@types';
 
 export async function createJob<T extends IOWithIntegrations<{ github: Autoinvoicing }>>(
   payload: PullRequestReviewEvent,
   io: T,
   ctx: TriggerContext
 ) {
-  const { action, pull_request, repository, organization, sender, review } = payload;
+  const { action, pull_request, repository, organization, review } = payload;
 
   switch (action) {
     case 'submitted': {
-      const contributor = await contributors.update(getContributorInfo(sender));
       await io.wait('wait for first call', 5);
 
       // store these events in gcloud
@@ -33,49 +32,9 @@ export async function createJob<T extends IOWithIntegrations<{ github: Autoinvoi
         review.state === 'changes_requested' ||
         review.state === 'commented'
       ) {
-        const event = {
-          action:
-            review.state === 'approved'
-              ? EventType.PR_REVIEW_APPROVE
-              : review.state === 'changes_requested'
-              ? EventType.PR_REVIEW_REJECT
-              : EventType.PR_REVIEW_COMMENT,
-          id: pull_request.number,
-          index: 1,
-          organization: organization?.login || 'holdex',
-          owner: pull_request.user.login,
-          repository: repository.name,
-          sender: review.user.login,
-          title: pull_request.title,
-          created_at: Math.round(new Date(pull_request.created_at).getTime() / 1000).toFixed(0),
-          updated_at: Math.round(new Date(pull_request.updated_at).getTime() / 1000).toFixed(0)
-        };
+        await insertPrEvent(payload, io);
 
-        // insert event for reviewer
-        await insertEvent(
-          event,
-          `${event.organization}/${event.repository}@${event.id}_${event.created_at}_${event.sender}_${event.action}`
-        );
-
-        // insert events for PR owner
-        const prOwnerEvent = Object.assign({}, event, {
-          sender: event.owner,
-          action:
-            review.state === 'approved'
-              ? EventType.PR_APPROVED
-              : review.state === 'changes_requested'
-              ? EventType.PR_REJECTED
-              : EventType.PR_COMMENTED
-        });
-        await insertEvent(
-          prOwnerEvent,
-          `${prOwnerEvent.organization}/${prOwnerEvent.repository}@${prOwnerEvent.id}_${prOwnerEvent.created_at}_${prOwnerEvent.sender}_${prOwnerEvent.action}`
-        );
-
-        const prInfo = await items.update(
-          await getPrInfo(pull_request, repository, organization, sender, contributor),
-          { onCreateIfNotExist: true }
-        );
+        const prInfo = await updatePrInfo(payload, io, (s) => s);
 
         const orgDetails = await io.github.runTask(
           'get org installation',
@@ -86,9 +45,16 @@ export async function createJob<T extends IOWithIntegrations<{ github: Autoinvoi
           { name: 'Get Organization installation' }
         );
 
-        const contributorList = await contributors.getManyBy({
-          id: { $in: prInfo.contributor_ids }
-        });
+        const contributorList = await io.github.runTask(
+          'get contributors list',
+          async () => {
+            const data = await contributors.getManyBy({
+              id: { $in: prInfo.contributor_ids }
+            });
+            return data;
+          },
+          { name: 'Get contributors list' }
+        );
 
         const taskChecks = [];
         for (const c of contributorList) {
@@ -119,4 +85,94 @@ export async function createJob<T extends IOWithIntegrations<{ github: Autoinvoi
       io.logger.log('current action for pull request is not in the parse candidate', payload);
     }
   }
+}
+
+async function insertPrEvent<
+  T extends IOWithIntegrations<{ github: Autoinvoicing }>,
+  E extends PullRequestReviewEvent = PullRequestReviewEvent
+>(payload: E, io: T) {
+  const { pull_request, repository, organization, review } = payload;
+
+  const event = {
+    action:
+      review.state === 'approved'
+        ? EventType.PR_REVIEW_APPROVE
+        : review.state === 'changes_requested'
+        ? EventType.PR_REVIEW_REJECT
+        : EventType.PR_REVIEW_COMMENT,
+    id: pull_request.number,
+    index: 1,
+    organization: organization?.login || 'holdex',
+    owner: pull_request.user.login,
+    repository: repository.name,
+    sender: review.user.login,
+    title: pull_request.title,
+    created_at: Math.round(new Date(pull_request.created_at).getTime() / 1000).toFixed(0),
+    updated_at: Math.round(new Date(pull_request.updated_at).getTime() / 1000).toFixed(0)
+  };
+
+  const eventId = `${event.organization}/${event.repository}@${event.id}_${event.created_at}_${event.sender}_${event.action}`;
+  await io.runTask(
+    `insert event: ${eventId}`,
+    async () => {
+      const data = await insertEvent(event, eventId);
+      return data;
+    },
+    { name: 'Insert Bigquery event' }
+  );
+
+  const prOwnerEvent = Object.assign({}, event, {
+    sender: event.owner,
+    action:
+      review.state === 'approved'
+        ? EventType.PR_APPROVED
+        : review.state === 'changes_requested'
+        ? EventType.PR_REJECTED
+        : EventType.PR_COMMENTED
+  });
+
+  const prOwnerEventId = `${prOwnerEvent.organization}/${prOwnerEvent.repository}@${prOwnerEvent.id}_${prOwnerEvent.created_at}_${prOwnerEvent.sender}_${prOwnerEvent.action}`;
+  await io.runTask(
+    `insert owner event: ${prOwnerEventId}`,
+    async () => {
+      const data = await insertEvent(prOwnerEvent, prOwnerEventId);
+      return data;
+    },
+    { name: 'Insert Bigquery owner event' }
+  );
+}
+
+async function updatePrInfo<
+  T extends IOWithIntegrations<{ github: Autoinvoicing }>,
+  E extends PullRequestReviewEvent = PullRequestReviewEvent
+>(payload: E, io: T, prepareInfo: (s: ItemSchema) => ItemSchema) {
+  const { pull_request, repository, organization, sender } = payload;
+  const contributorInfo = getContributorInfo(sender);
+
+  const contributor = await io.runTask<any>(
+    `update contributor: ${contributorInfo._id}`,
+    async () => {
+      const data = await contributors.update(contributorInfo);
+      return data;
+    },
+    { name: 'Update Contributor schema' }
+  );
+
+  const prInfo = await io.runTask<any>(
+    `get pr info: ${pull_request.node_id}`,
+    async () => {
+      const data = await getPrInfo(pull_request, repository, organization, sender, contributor);
+      return data;
+    },
+    { name: 'Get Item schema' }
+  );
+
+  return io.runTask<any>(
+    `update pr info: ${pull_request.node_id}`,
+    async () => {
+      const data = await items.update(prepareInfo(prInfo), { onCreateIfNotExist: true });
+      return data;
+    },
+    { name: 'Update Item schema' }
+  );
 }
