96d77313e2567540dbd848e24ff44256fff114a0
teodorus-nathaniel <nathankurnia26@gmail.com>
2024-11-19 04:07:40 +0700

    feat: not do anything for draft pr and pr body edit event

--- src/lib/server/trigger-dev/fix-pr.ts
+++ src/lib/server/trigger-dev/fix-pr.ts
@@ -40,20 +40,35 @@ export async function runPrFixCheckRun<
 >(payload: E, io: T) {
   const { repository, organization, sender } = payload;
   let title;
+  let isPullRequestEvent = false;
   if ('pull_request' in payload) {
+    isPullRequestEvent = true;
     title = payload.pull_request.title;
   } else {
     title = payload.issue.title;
   }
 
-  const isTitleChangedFromFixPr =
-    payload.action === 'edited' &&
-    'title' in payload.changes &&
-    fixPrRegex.test(payload.changes.title?.from ?? '') &&
-    !fixPrRegex.test(title);
-  const isFixPr = fixPrRegex.test(title);
+  let shouldRunFixPrCheck = false;
+  if (fixPrRegex.test(title)) {
+    shouldRunFixPrCheck = true;
+    if (isPullRequestEvent && payload.action === 'edited') {
+      // if the pr's body is edited, then don't need to do anything
+      if ('body' in payload.changes) {
+        shouldRunFixPrCheck = false;
+      }
+    }
+  }
 
-  if (!isFixPr && !isTitleChangedFromFixPr) {
+  let shouldDeleteFixPrCheck = false;
+  if (isPullRequestEvent && payload.action === 'edited') {
+    if ('title' in payload.changes) {
+      if (fixPrRegex.test(payload.changes.title?.from ?? '') && !fixPrRegex.test(title)) {
+        shouldDeleteFixPrCheck = true;
+      }
+    }
+  }
+
+  if (!shouldRunFixPrCheck && !shouldDeleteFixPrCheck) {
     return;
   }
   if (!organization) {
@@ -86,8 +101,12 @@ export async function runPrFixCheckRun<
     { name: 'Get Organization installation' }
   );
 
-  if (isFixPr) {
+  if (shouldRunFixPrCheck) {
     const { user } = pullRequest;
+
+    if (pullRequest.draft) {
+      return io.logger.log('skipped draft pull request');
+    }
 
     if (payload.action !== 'closed') {
       await io.runTask(
@@ -112,7 +131,7 @@ export async function runPrFixCheckRun<
     } else if (payload.action === 'closed' && payload.pull_request.merged) {
       await processBugReport(orgDetails.id, organization.login, repository.name, pullRequest, io);
     }
-  } else if (isTitleChangedFromFixPr) {
+  } else if (shouldDeleteFixPrCheck) {
     await deleteFixPrReportAndResolveCheckRun(
       orgDetails.id,
       organization.login,
