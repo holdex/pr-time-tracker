3f73fd5e9d7aa8fcd38e23a893d274091d28b452
teodorus-nathaniel <nathankurnia26@gmail.com>
2025-01-17 16:45:54 +0700

    chore: disable onIssue and onIssueLabel job

--- src/lib/server/trigger-dev/jobs/index.ts
+++ src/lib/server/trigger-dev/jobs/index.ts
@@ -24,6 +24,7 @@ config.integrationsList.forEach((org) => {
       event: events.onIssue,
       org: org.name
     }),
+    enabled: false,
     integrations: { github },
     run: async (payload, io, ctx) =>
       createIssueCreationJob<IOWithIntegrations<{ github: Autoinvoicing }>>(payload, io, ctx, org)
@@ -52,6 +53,7 @@ config.integrationsList.forEach((org) => {
       event: events.onIssueLabel,
       org: org.name
     }),
+    enabled: false,
     integrations: { github },
     run: async (payload, io, ctx) =>
       createIssueJob<IOWithIntegrations<{ github: Autoinvoicing }>>(payload, io, ctx, org)
