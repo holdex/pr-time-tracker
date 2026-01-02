f4d768568f28e1a02b925d0c0b4f6513fefaea6e
Teodorus Nathaniel <tnathanielk@gmail.com>
2026-01-02 22:24:33 +0700

    feat: redirect from /docs

---
+++ src/routes/docs/+page.server.ts
@@ -0,0 +1,18 @@
+import { redirect } from '@sveltejs/kit';
+
+import type { PageServerLoad } from './$types';
+
+import { routes } from '$lib/config';
+import { REDIRECT_TEMP } from '$lib/constants';
+
+import { UserRole } from '$lib/@types';
+
+export const load: PageServerLoad = async ({ parent }) => {
+  const data = await parent();
+
+  if (data.user?.role === UserRole.MANAGER || data.user?.email === 'tnathanielk@gmail.com') {
+    throw redirect(REDIRECT_TEMP, routes.managerCommandsDocs.path);
+  }
+
+  throw redirect(REDIRECT_TEMP, routes.prs.path);
+};
